5ebd7174648cb5e804b81155df6ea5a267ba62cf
unknown <sk222sw@NY108X07.lnu.se>
2015-11-11 15:53:03 +0100

    removed some unused code and global variable

--- scraper/site/modules/scraper.js
+++ scraper/site/modules/scraper.js
@@ -4,21 +4,17 @@ var request = require("request");
 var cheerio = require("cheerio");
 
 var url = "http://localhost:8080";
-// exports.scrape = "hej";
 
 requestp(url)
-	.then(function (data) {
-    	saker = data;
-	})
-	.then(function(){
-		console.log(saker);
-		exports.scrape = saker;
+	.then(function(data){
+		console.log(data);
+		exports.scrape = data;
 	});
 
 function requestp(url) {
     return new promise(function (resolve, reject) {
-        request(url, function (err, res, body) {
-            resolve(body);
+        request(url, function (err, res, html) {
+            resolve(html);
         });
     });
 }
