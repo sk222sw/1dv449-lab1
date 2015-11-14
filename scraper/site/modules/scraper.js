57a3f9f9fac2721de33e364e87dda1b5ab2223db
Sonny <sk222sw@student.lnu.se>
2015-11-14 12:18:53 +0100

    nothing added

--- scraper/site/modules/scraper.js
+++ scraper/site/modules/scraper.js
@@ -1,5 +1,4 @@
 var promise = require('promise');
-var promise = require("promise");
 var request = require("request");
 var cheerio = require("cheerio");
 
@@ -15,7 +14,6 @@ requestp(url)
 		var $ = cheerio.load(html);
 		var listElements = [];
 
-
 		$("a").each(function() {
 			listElements.push($(this).attr("href"));
 		});
@@ -23,7 +21,7 @@ requestp(url)
 		scrapeLink(listElements[0]);
 
 		exports.scrape = "nothing to show yet";
-	});
+});
 
 
 
@@ -40,7 +38,6 @@ function requestp(url) {
 
 /////////////
 // scrape a link with href route
-// 
 function scrapeLink(href) {
 
 	var link = url + href;
@@ -69,27 +66,46 @@ function scrapeLink(href) {
 
 function scrapeCalendar(html, link) {
 	var personLinks = getHtml.getElements(html, "a", "href");
+	var persons = [];
 
-	requestp(link + "/" + personLinks[0])
-		.then(function(html) {
-			scrapePerson(html);
-		});
+	var pr = new promise(function (resolve, reject){
+		for (var i = 0; i < personLinks.length; i++) {
+			requestp(link + "/" + personLinks[i])
+				persons.push(scrapePerson(html));
+				console.log(personLinks[i]);
+				resolve(scrapePerson(html));
+		}
+	});
+
+	pr.then(function(){
+		console.log(persons);
+	})
+
+
+
 }
 
 function scrapePerson(html) {
 	var days = [];
 	var ok = [];
+
+	var name = getHtml.getElementsText(getHtml.getElements(html, "h2", false));
+	days = getHtml.getElementsText(getHtml.getElements(html, "thead tr th", false));
+	ok = getHtml.getElementsText(getHtml.getElements(html, "tbody tr td", false));
+
+	var person = {name: name, days: days, ok: ok};
+
+	return person;
+}
+
+function getOkDays(days, ok) {
+
 	var okDays = [];
-
-	console.log(html);
-
-	days = getHtml.getElements(html, "thead tr th", false);
-	ok = getHtml.getElements(html, "tbody tr td", false);
-
-	getHtml.getElementsText(days);
-
 	for (var i = 0; i < days.length; i++) {
-		console.log(days[i].text());
+		if(ok[i].toLowerCase().trim() === "ok"){
+			okDays[i] = true;
+		} else {
+			okDays[i] = false;
+		}
 	}
-
 }
